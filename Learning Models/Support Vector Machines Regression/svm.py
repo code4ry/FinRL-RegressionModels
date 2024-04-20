c846b907d4872976258cc9bd5bf1ffefaa6ef23b
Ameyabarve123 <barveameya@yahoo.com>
2024-04-20 18:14:23 -0400

    added code to train model on on dataset, and test on another dataset. I also plotted this test data

--- Learning Models/Support Vector Machines Regression/svm.py
+++ Learning Models/Support Vector Machines Regression/svm.py
@@ -5,7 +5,7 @@ from sklearn import model_selection
 from statsmodels.tools.eval_measures import mse
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
-
+import matplotlib.pyplot as plt
 df1Name = input("What is the name of your first dataset?: ")
 df = pd.read_csv("/content/" + df1Name)
 
@@ -38,3 +38,38 @@ meanErrorTrain = mean_absolute_error(yTrain, yPredTrain)
 
 print(meanErrorTest)
 print(meanErrorTrain)
+
+plt.scatter(yTest, yPredTest)
+plt.xlabel("Actual Target Values")
+plt.ylabel("Predicted Target Values")
+plt.title("Actual vs. Predicted (Testing Set)")
+plt.show()
+# train model on one dataset, and test on another
+df2Name = input("What is the name of your second dataset?: ")
+df2 = pd.read_csv("/content/" + df2Name)
+
+df2 = df2.dropna()
+df2 = df2.dropna(axis=1)
+df2 = df2.dropna(how='all')
+
+X2 = df2[['Open', 'High', 'Low']]
+y2 = df2[['Adj Close']]
+
+xTrain2, xTest2, yTrain2, yTest2 = train_test_split(X2, y2, test_size=0.3, random_state=42)
+
+lk = svm.SVR()
+lk.fit(xTrain, yTrain)
+
+yPredTest2 = lk.predict(xTest2)
+meanErrorTest = mean_absolute_error(yTest2, yPredTest2)
+print(meanErrorTest)
+
+yPredTrain2 = lk.predict(xTrain2)
+meanErrorTrain = mean_absolute_error(yTrain2, yPredTrain2)
+print(meanErrorTrain)
+
+plt.scatter(yTest2, yPredTest2)
+plt.xlabel("Actual Target Values")
+plt.ylabel("Predicted Target Values")
+plt.title("Actual vs. Predicted (Testing Set)")
+plt.show()
